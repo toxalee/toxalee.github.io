3c8aab51411806cf21898b8c3eb9a56b8b43f622
toxalee <106753980+toxalee@users.noreply.github.com>
2022-07-10 21:12:51 +0300

    Add files via upload

---
+++ start/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/Arrow 2.png" alt="arrow-left">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="/images/Arrow 1.png" alt="arrow-right">',
+    });
+});
